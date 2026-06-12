3d4b87a16bf0577f34e9feceed95c9b094a61564
Kumar Kartikeya Dwivedi <memxor@gmail.com>
2026-06-12 03:55:03 +0200

    scx_mitosis: Interlock LLC DSQ draining

--- scheds/rust/scx_mitosis/src/bpf/intf.h
+++ scheds/rust/scx_mitosis/src/bpf/intf.h
@@ -89,6 +89,7 @@ struct cpu_ctx {
 	u64 vtime_now;
 	u32 cell;
 	u32 llc;
+	u32 llc_drain_seq;
 };
 
 struct cgrp_ctx {
@@ -136,6 +137,9 @@ struct cell {
 	u64 owner_cgid;
 	// Whether or not the cell is used
 	u32 in_use;
+
+	// LLC DSQs that need draining because this cell has no CPUs there
+	u64 llcs_to_drain;
 
 	// Per-LLC data (cacheline-aligned)
 	struct cell_llc llcs[MAX_LLCS];
